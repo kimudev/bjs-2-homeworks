db5dd76e246d3af72c9fc43484439900d694a4a9
kimudev <kimudev@hotmail.com>
2024-02-13 15:08:31 +0500

    Поправил функцию getAverage

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -36,9 +36,8 @@ Student.prototype.addMarks = function (...marks) {
 Student.prototype.getAverage = function () {
     if ('marks' in this) {
         let sum = this.marks.reduce((acc, curr) => acc + curr, 0);
-        return sum / this.marks.length;
-        
-    } else if (this.marks.length = 0) {
+        return sum / Number(this.marks.length);
+    } else if (this.marks.length === 0) {
         return 0;
     } else {
         return 0;
